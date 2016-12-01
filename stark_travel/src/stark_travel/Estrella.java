c6e17accbca75246cd6e2d59bd438c0ecdf25375
admin <admin@192.168.1.132>
2016-11-30 18:51:15 -0600

    Hotfix Clases and frame

--- stark_travel/src/stark_travel/Estrella.java
+++ stark_travel/src/stark_travel/Estrella.java
@@ -9,13 +9,6 @@ package stark_travel;
  *
  * @author Romero
  */
-public class Stark_travel {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Estrella {
     
 }
